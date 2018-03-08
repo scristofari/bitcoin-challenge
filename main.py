0c6d27197aa42fe563618cb52ff033f4eed846e0
Sylvain Cristofari <s.cristofari@gmail.com>
2018-03-08 11:06:45 +0100

    up

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class BitCoinChallenge(cmd.Cmd):
         engine.generate_spot_data()
 
     def do_test(self, arg):
-        data = engine.load_data('BTC-EUR')
+        data = engine.load_data('BTC-USD')
         X_train, X_test, y_train, y_test, scaler_x, scaler_y = engine.prepare(data)
         model, history = engine.train(X_train, X_test, y_train, y_test)
         engine.test_order_percent(data, model, scaler_x, scaler_y)
